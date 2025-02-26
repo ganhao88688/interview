b404c0e3e700ba89a7a525fab06c9a0d53f13af8
ganhao <ganhao1687@gmail.com>
2025-02-26 20:14:12 +0800

    can use delete button but at least one

--- src/components/ConsumeItem.js
+++ src/components/ConsumeItem.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const ConsumeItem = ({
   index,
   totalCost,
@@ -8,6 +10,7 @@ const ConsumeItem = ({
 }) => {
   let item = formDataItems[index];
   let cost = item.cost;
+  let [size, setSize] = useState(window.innerWidth / 100);
   let costChange = (e) => {
     if (e.target.value) {
       //nan
@@ -21,22 +24,35 @@ const ConsumeItem = ({
       });
     }
   };
+  setInterval(() => setSize(window.innerWidth / 100), 500);
   return (
     <div className="formOption">
-      <label>項目名稱:</label>
-      <input placeholder="項目名稱" defaultValue={item.name}></input>
-      <label>金額:</label>
-      <input
-        type="number"
-        placeholder="金額"
-        min="0"
-        defaultValue={cost}
-        onChange={costChange}
-      ></input>
-      <label>評分:</label>
-      <input placeholder="1~5" list="foodScore"></input>
+      <div className="item">
+        <label>項目名稱:</label>
+        <input
+          placeholder="項目名稱"
+          defaultValue={item.name}
+          size={size}
+        ></input>
+      </div>
+      <div className="item">
+        <label>金額:</label>
+        <input
+          type="number"
+          placeholder="金額"
+          min="0"
+          defaultValue={cost}
+          style={{ width: size * 10 + 5 }}
+          onChange={costChange}
+        ></input>
+      </div>
+      <div className="item">
+        <label>評分:</label>
+        <input placeholder="1~5" size={size} list="foodScore"></input>
+      </div>
       <button
-        onClick={() => {
+        onClick={(e) => {
+          e.preventDefault();
           deleteItem(index);
           console.log("in delete button");
         }}
--- src/pages/CalculateScore.js
+++ src/pages/CalculateScore.js
@@ -15,36 +15,44 @@ const CalculateScore = () => {
     setFormDataItems(tempArray);
   };
   let deleteItem = (index) => {
-    console.log(formDataItems.splice(index, 1));
-    setTotalCost(totalCost - formDataItems[index].cost);
+    // setTotalCost(totalCost - formDataItems[index].cost);
     setFormDataItems(formDataItems.splice(index, 1));
-    setFormCount(formCount - 1);
+
+    console.log(`Form Data Items:${JSON.stringify(formDataItems)}`);
+    // setFormCount(formCount - 1);
   };
   return (
     <div className="CalculateScore">
       <h1>消費紀錄</h1>
       <form>
-        {formDataItems &&
-          formDataItems.map((item, index) => {
-            return (
-              <ConsumeItem
-                index={index}
-                item={item}
-                totalCost={totalCost}
-                setTotalCost={setTotalCost}
-                formDataItems={formDataItems}
-                setFormDataItems={setFormDataItems}
-                deleteItem={deleteItem}
-              />
-            );
-          })}
+        {formDataItems.map((item, index) => {
+          return (
+            <ConsumeItem
+              key={index}
+              index={index}
+              item={item}
+              totalCost={totalCost}
+              setTotalCost={setTotalCost}
+              formDataItems={formDataItems}
+              setFormDataItems={setFormDataItems}
+              deleteItem={deleteItem}
+            />
+          );
+        })}
       </form>
       <div className="result">
         <label>總金額:</label>
         <input type="number" value={totalCost} disabled></input>
       </div>
       <div className="button">
-        <button onClick={addItem}>新增</button>
+        <button
+          onClick={(e) => {
+            addItem();
+            e.preventDefault();
+          }}
+        >
+          新增
+        </button>
         <button>提交</button>
       </div>
       <datalist id="foodScore">
